78f32dbce328bc34923f99c3bbd0ace33cfa0192
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-06-09 00:04:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import asyncio
 import requests
 import subprocess
 from modules import helper
-from utils import progress_bar
-from vars import api_id, api_hash, bot_token
+from modules.utils import progress_bar
+from modules.vars import api_id, api_hash, bot_token
 from aiohttp import ClientSession
 from pyromod import listen
 from subprocess import getstatusoutput
