92bbd221161a0a4a852d5e5ad9a55021aa9249e2
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-06-08 23:50:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 import asyncio
 import requests
 import subprocess
-import core as helper
+import helper
 from utils import progress_bar
 from vars import api_id, api_hash, bot_token
 from aiohttp import ClientSession
